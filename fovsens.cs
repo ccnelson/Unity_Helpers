adfad74bacd2fa56e6a5ea13dea7a490b31e793b
ccnelson <46001311+ccnelson@users.noreply.github.com>
2020-09-10 01:11:07 +0100

    Initial commit

---
+++ fovsens.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEngine;
+
+public class fovsens : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject player;
+
+    [SerializeField]
+    public bool playerInSight = false;
+
+
+    void Update()
+    {
+        // player object is within vision cone of this object
+        playerInSight = Vector3.Dot(transform.forward, (player.transform.position - transform.position).normalized) > 0.5f;
+
+    }
+}
