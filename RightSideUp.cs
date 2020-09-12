151794eb0e4efad73e30fcd80b229b7aa53777b4
ccnelson <46001311+ccnelson@users.noreply.github.com>
2020-09-12 18:22:12 +0100

    Sublime text curly brace indentation error

--- RightSideUp.cs
+++ RightSideUp.cs
@@ -10,8 +10,8 @@ public class RightSideUp : MonoBehaviour
 	public void TestAlignment()
 	{
 		if (Vector3.Dot(player.transform.up, Vector3.down) > 0)
-        {
-        	Debug.Log("Ooh what a feeling");
-        }
+		{
+			Debug.Log("Ooh what a feeling");
+		}
 	}
 }
